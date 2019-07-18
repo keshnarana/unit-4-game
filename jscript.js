88be33c7f5ddf68092afd095df48de488faa352d
keshnarana <50150544+keshnarana@users.noreply.github.com>
2019-07-17 21:12:59 -0400

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -1,17 +1,17 @@
-var crystalValues = {};
+var crystal = [];
 var userTotal= 0;
 var random;
 
 function game(){
 
-crystalValues[1] = Math.floor(Math.random()*12+1);              //giving random numb. between 1-12 to the 4 crystals
-console.log(crystalValues[1])
-crystalValues[2] = Math.floor(Math.random()*12+1);
-console.log(crystalValues[2])
-crystalValues[3] = Math.floor(Math.random()*12+1);
-console.log(crystalValues[3])
-crystalValues[4] = Math.floor(Math.random()*12+1);
-console.log(crystalValues[4])
+crystal[1] = Math.floor(Math.random()*12+1);              //giving random numb. between 1-12 to the 4 crystals
+console.log(crystal[1])
+crystal[2] = Math.floor(Math.random()*12+1);
+console.log(crystal[2])
+crystal[3] = Math.floor(Math.random()*12+1);
+console.log(crystal[3])
+crystal[4] = Math.floor(Math.random()*12+1);
+console.log(crystal[4])
 
 
 random =  Math.floor(Math.random()*(120-19)+19);             // random num. btwn 19-120 to lucky numb.
@@ -50,10 +50,10 @@ function reset(){
 game();                                                       //start
 
 
-function getCrystalHandler(crystalKey) {                       //function for score increment on click
+function getCrst(i) {                       //function for score increment on click
     return function() {
        
-     userTotal = userTotal + crystalValues[crystalKey];
+     userTotal = userTotal + crystal[i];
      console.log("New userTotal " + userTotal);
      $("#score").text(userTotal);
  
@@ -71,7 +71,7 @@ function getCrystalHandler(crystalKey) {                       //function for sc
 }
 
 
-$("#image1").on("click", getCrystalHandler(1));
-$("#image2").on("click", getCrystalHandler(2));
-$("#image3").on("click", getCrystalHandler(3));
-$("#image4").on("click", getCrystalHandler(4));
+$("#image1").on("click", getCrst(1));
+$("#image2").on("click", getCrst(2));
+$("#image3").on("click", getCrst(3));
+$("#image4").on("click", getCrst(4));
